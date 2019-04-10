77ebc35f7e203ac94bc8f1c4465ff9b774371a11
NVANH97 <hoangvietanhnguyen@gmail.com>
2019-04-10 15:26:36 +0700

    first

--- BTL.WS/Content/JS/SignUp.js
+++ BTL.WS/Content/JS/SignUp.js
@@ -2,38 +2,66 @@
     btn_signup.Signup();
 });
 
+function Acc() {
+    this.firstName = '';
+    this.lastName = '';
+    this.email = '';
+    this.password = '';
+    this.confirmPassword = '';
+    this.gender = '';
+    this.birthday = '';
+
+    this.setInfor = function (firstName, lastName, email, password, confirmPassword, gender, birthday) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.confirmPassword = confirmPassword;
+        this.gender = gender;
+        this.birthday = birthday;
+    }
+}
+
 var btn_signup = {
     Signup: function () {
         $('#btn-signup').click(function () {
-            var acc = {};
-            acc.firstName = $('#firstname').val();
-            acc.lasrName = $('#lastname').val();
-            acc.email = $('#email').val();
-            acc.password = $('#password').val();
-            acc.confirmPassword = $('#confirm_password').val();
-            $('input[type="radio"]').click(function () {
-                if ($(this).is(':checked')) {
-                    var result = $(this).val();
-                    if (result == 'M') {
-                        acc.gender = 1;
-                    } else {
-                        acc.gender = 0;
-                    }
+            var acc = new Acc();            
+            var firstname = $('#firstname').val();
+            var lastname = $('#lastname').val();
+            var email = $('#email').val();
+            var password = $('#password').val();
+            var confirm = $('#confirm_password').val();
+            var birthday = new Date();
+            birthday.setDate($('#day').val());
+            birthday.setMonth($('#month').val());
+            birthday.setFullYear($('#year').val());
+            var gender = new Int32Array();
+            $('input:radio[name="gender"]').change(function () {
+                debugger
+                if ($(this).val() == 'Female') {
+                    gender = 0;
+                    
+                }
+                else {
+                    gender = 1;
                 }
             });
-            $.ajax({
-                url: "http://localhost:53113/Account/post",
-                type: "POST",
-                async: false,
-                data: JSON.stringify(acc),
-                dataType:"json",
-                success: function (data) {
-                    alert("ok");
-                },
-                error: function (data) {
-                    alert(data.status);
-                }
-            });
+            
+            alert(gender);
+            acc.setInfor();
+            //$.ajax({
+            //    url: "http://localhost:53113/Account/post",
+            //    type: "POST",
+            //    async: false,
+            //    data: JSON.stringify(acc),
+            //    dataType:"json",
+            //    success: function (data) {
+            //        alert("ok");
+            //    },
+            //    error: function (data) {
+            //        alert(data.status);
+            //    }
+            //});
         });
     }
 }
